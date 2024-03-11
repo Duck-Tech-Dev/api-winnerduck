6b09c30a3a0182f73559fc4d157f1ef5d75aa02c
Erthium <ertyum35@gmail.com>
2024-03-11 11:20:30 +0300

    feat: create signup route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,3 @@
-import { Controller } from '@nestjs/common';
 import { Controller, HttpCode, HttpStatus, Post, Body} from '@nestjs/common';
 import { AuthService } from './auth.service';
 
@@ -7,8 +6,13 @@ class SignInDto {
     password: string;
 }
 
+class SignUpDto {
+    username: string;
+    email: string;
+    password: string;
+}
+
 @Controller('auth')
-export class AuthController {}
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
     
@@ -17,5 +21,12 @@ export class AuthController {
     async login(@Body() signInDto: SignInDto): Promise<{access_token: string}>{
         const { username, password } = signInDto;
         return this.authService.signIn(username, password);
+    }
+
+    @HttpCode(HttpStatus.CREATED)
+    @Post('signup')
+    async signUp(@Body() signInDto: SignUpDto): Promise<{access_token: string}>{
+        const { username, email, password } = signInDto;
+        return this.authService.signUp(username, email, password);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -18,7 +18,23 @@ export class AuthService {
         }
         else {
             throw new Error('Invalid username or password');
+        }   
+    }
+
+    async signUp(username: string, email: string, password: string): Promise<{access_token: string}> {
+        const user: User = await this.userService.getByUsername(username);
+        if (user) {
+            throw new Error('Username already exists');
         }
-        
+        const emailUser: User = await this.userService.getByEmail(email);
+        if (emailUser) {
+            throw new Error('Email already exists');
+        }
+        else {
+            const user: User = await this.userService.createUser(username, email, password);
+            const payload = { username: user.username, sub: user.id };
+            const token = await this.jwtService.signAsync(payload);
+            return { access_token: token };
+        }
     }
 }
