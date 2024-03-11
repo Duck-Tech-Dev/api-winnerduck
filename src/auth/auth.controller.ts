2417e6712f4ec985c9bfdddeee04399f9beeb20b
Erthium <ertyum35@gmail.com>
2024-03-11 11:20:13 +0300

    feat: add signin route to auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
 import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, Body} from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+class SignInDto {
+    username: string;
+    password: string;
+}
 
 @Controller('auth')
 export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+    
+    @HttpCode(HttpStatus.OK)
+    @Post('signin')
+    async login(@Body() signInDto: SignInDto): Promise<{access_token: string}>{
+        const { username, password } = signInDto;
+        return this.authService.signIn(username, password);
+    }
+}
