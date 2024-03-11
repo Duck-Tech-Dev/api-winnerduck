db74af194bd4e5b4860122005688cebccb488f0d
Erthium <ertyum35@gmail.com>
2024-03-11 11:20:59 +0300

    feat: create an auth guard as in nest js docs

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,11 +1,31 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
-  }
+    constructor(private readonly jwtService: JwtService) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const token = await this.extractToken(request);
+        if (!token) {
+            throw new UnauthorizedException();
+        }
+        try {
+            const payload = await this.jwtService.verifyAsync(token, { secret: jwtConstants.secret } );
+            request['user'] = payload;
+        } 
+        catch {
+            throw new UnauthorizedException();
+        }
+        return true;    
+    }
+
+    async extractToken(request: Request): Promise<string | undefined> {
+        const [type, token] = request.headers.authorization?.split(' ') ?? [];
+        return type === 'Bearer' ? token : undefined;
+    }
 }
