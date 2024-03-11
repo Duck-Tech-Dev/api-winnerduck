fd5bd66b40d8f37200ba15067c4dbb4bb4c2c882
Erthium <ertyum35@gmail.com>
2024-03-11 10:40:43 +0300

    feat: create AuthService async signIn and creation of access token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UserService } from 'src/user/user.service';
+import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: jwtConstants.expiresIn },
+    }), 
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User, UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService
+    ) {}
+
+    async signIn(username: string, password: string): Promise<{access_token: string}> {
+        const user: User = await this.userService.getByUsername(username);
+        if (user && user.password === password) {
+            const payload = { username: user.username, sub: user.id };
+            const token = await this.jwtService.signAsync(payload);
+            return { access_token: token };
+        }
+        else {
+            throw new Error('Invalid username or password');
+        }
+        
+    }
+}
