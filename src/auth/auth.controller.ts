5ea20dab6ecd404cf7413614511104832f5d852f
Erthium <ertyum35@gmail.com>
2024-03-11 12:32:26 +0300

    fix: add error codes to auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, HttpCode, HttpStatus, Post, Body} from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, Body, HttpException} from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 class SignInDto {
@@ -19,14 +19,27 @@ export class AuthController {
     @HttpCode(HttpStatus.OK)
     @Post('signin')
     async login(@Body() signInDto: SignInDto): Promise<{access_token: string}>{
-        const { username, password } = signInDto;
-        return this.authService.signIn(username, password);
+        try {
+            const { username, password } = signInDto;
+            return this.authService.signIn(username, password);
+        }
+        catch (error) {
+            console.log(error);
+            throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @HttpCode(HttpStatus.CREATED)
     @Post('signup')
-    async signUp(@Body() signInDto: SignUpDto): Promise<{access_token: string}>{
-        const { username, email, password } = signInDto;
-        return this.authService.signUp(username, email, password);
+    async signUp(@Body() signInDto: SignUpDto): Promise<{access_token: string} | {error: string}>{
+        try {
+            const { username, email, password } = signInDto;
+            console.log(username, email, password);
+            return this.authService.signUp(username, email, password);
+        }
+        catch (error) {
+            console.log(error);
+            throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+        }
     }
 }
