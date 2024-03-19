a7009e5ff216835c269cbecd34a03108af2828a0
Erthium <ertyum35@gmail.com>
2024-03-20 00:09:14 +0300

    fix: resolve merge conflicts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,72 +1,73 @@
-import { Controller, HttpCode, HttpStatus, Post, Body, Param, UseGuards, Get, Res} from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, Body, UseGuards, Get, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
 import { Response } from 'express';
 
 class LogInDto {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
 class SignUpDto {
-    username: string;
-    email: string;
-    password: string;
+  username: string;
+  email: string;
+  password: string;
 }
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
-    
-    @HttpCode(HttpStatus.OK)
-    @Post('login')
-    async login(@Body() logInDto: LogInDto, @Res({passthrough: true}) response: Response): Promise<void> {
-        try {
-            const { username, password } = logInDto;
-            const { access_token } = await this.authService.logIn(username, password);
-            response.cookie('wd_access_token', access_token, {httpOnly: true, secure: true});
-            response.send();
-        }
-        catch (error) {
-            console.log("Error on auth/login:\n", error);
-            response.status(HttpStatus.BAD_REQUEST).send(error.message);
-        }
+  constructor(private readonly authService: AuthService) {}
+  
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async login(@Body() logInDto: LogInDto, @Res({passthrough: true}) response: Response): Promise<void> {
+    try {
+      const { username, password } = logInDto;
+      const { access_token } = await this.authService.logIn(username, password);
+      response.cookie('wd_access_token', access_token, {httpOnly: true, secure: true});
+      response.send();
     }
+    catch (error) {
+      console.log("Error on auth/login:\n", error);
+      response.status(HttpStatus.BAD_REQUEST).send(error.message);
+    }
+  }
 
-    @HttpCode(HttpStatus.CREATED)
-    @Post('signup')
-    async signUp(@Body() signInDto: SignUpDto, @Res({passthrough: true}) response: Response): Promise<void> {
-        try {
-            const { username, email, password } = signInDto;
-            const { access_token } = await this.authService.signUp(username, email, password);
-            response.cookie('wd_access_token', access_token, {httpOnly: true, secure: true});
-            response.send();
-        }
-        catch (error) {
-            console.log("Error on auth/signup:\n", error);
-            response.status(HttpStatus.BAD_REQUEST).send(error.message);
-        }
+  @HttpCode(HttpStatus.CREATED)
+  @Post('signup')
+  async signUp(@Body() signInDto: SignUpDto, @Res({passthrough: true}) response: Response): Promise<void> {
+    try {
+      const { username, email, password } = signInDto;
+      const { access_token } = await this.authService.signUp(username, email, password);
+      response.cookie('wd_access_token', access_token, {httpOnly: true, secure: true});
+      response.send();
     }
+    catch (error) {
+      console.log("Error on auth/signup:\n", error);
+      response.status(HttpStatus.BAD_REQUEST).send(error.message);
+    }
+  }
 
-    @UseGuards(AuthGuard)
-    @Get('check')
-    async checkAuth(): Promise<void> {
-        return;
-    }
+  @UseGuards(AuthGuard)
+  @Get('check')
+  async checkAuth(): Promise<void> {
+      return;
+  }
 
-    @HttpCode(HttpStatus.OK)
-    @Post('validate')
-    async validateToken(@Body() validationDto: any, @Res({passthrough: true}) response: Response): Promise<void> {
-        try {
-            const allCookies = validationDto.cookies;
-            const token = allCookies.find((cookie: any) => cookie.name === 'wd_access_token').value;
-            await this.authService.validateToken(token);
-            response.send();
-        }
-        catch (error) {
-            console.log("Error on auth/validate:\n", error);
-            response.status(HttpStatus.BAD_REQUEST).send(error.message);
-        }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('validate')
+  async validateToken(@Body() validationDto: any, @Res({passthrough: true}) response: Response): Promise<void> {
+    try {
+      const allCookies = validationDto.cookies;
+      const token = allCookies.find((cookie: any) => cookie.name === 'wd_access_token').value;
+      await this.authService.validateToken(token);
+      response.send();
     }
+    catch (error) {
+      console.log("Error on auth/validate:\n", error);
+      response.status(HttpStatus.BAD_REQUEST).send(error.message);
+    }
+  }
 
 }
--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -4,39 +4,35 @@ import { jwtConstants } from './constants';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private readonly jwtService: JwtService) {}
+  constructor(private readonly jwtService: JwtService) {}
 
-    async canActivate(context: ExecutionContext): Promise<boolean> {
-        const request = context.switchToHttp().getRequest();
-        const token = await this.extractToken(request);
-        if (!token) {
-            throw new UnauthorizedException();
-        }
-        try {
-            const payload = await this.jwtService.verifyAsync(token, { secret: jwtConstants.secret } );
-            request['user'] = payload;
-        } 
-        catch (e) {
-            throw new UnauthorizedException();
-        }
-        return true;
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = await this.extractToken(request);
+    if (!token) {
+      throw new UnauthorizedException();
     }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, { secret: jwtConstants.secret });
+      request['user'] = payload;
+    }
+    catch (e) {
+      console.log(e);
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
 
-    async extractToken(request: any): Promise<string | undefined> {
-        try {
-            const cookies: string = request.headers.cookie;
-            if (!cookies) {
-                return undefined;
-            }
-            const tokenCookie = cookies.split(';').find(c => c.trim().startsWith('wd_access_token='));
-            if (!tokenCookie) {
-                return undefined;
-            }
-            const token = tokenCookie.split('=')[1];
-            return token;
-        }
-        catch {
-            return undefined;
-        }
+  async extractToken(request: any): Promise<string | undefined> {
+    const cookies: string = request.headers.cookie;
+    if (!cookies) {
+      return undefined;
     }
+    const tokenCookie = cookies.split(';').find(c => c.trim().startsWith('wd_access_token='));
+    if (!tokenCookie) {
+      return undefined;
+    }
+    const token = tokenCookie.split('=')[1];
+    return token;
+  }
 }
