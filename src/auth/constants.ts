5297fcde3dea9212e8fe222ac59034e2c2bdbda1
Erthium <ertyum35@gmail.com>
2024-03-23 03:01:08 +0300

    feat: put cookie options in constants.ts

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,4 +1,13 @@
+import { CookieOptions } from "express";
+
 export const jwtConstants = {
   secret: 'TODO: change me and secure me in a safe place',
   expiresIn: '6h'
+};
+
+export const cookieOptions: CookieOptions = {
+  httpOnly: true,
+  secure: true,
+  priority: "high",
+  maxAge: 21600000
 };
