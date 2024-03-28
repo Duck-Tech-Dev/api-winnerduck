8d9b34a2ef8e53d34302228e2271af165ce8919e
Hüseyin Şimşek <huseyinsimsek7904@gmail.com>
2024-03-28 16:14:05 +0300

    fix: fix mistake in postgres.service.ts

--- src/postgres/postgres.service.ts
+++ src/postgres/postgres.service.ts
@@ -8,9 +8,9 @@ export class PostgresService {
   constructor() {
     this.pool = new Pool({
       database: process.env.DB_NAME,
-      host: process.env.DB_P_HOST,
-      port: process.env.DB_P_PORT,
-      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD
     });
   }
