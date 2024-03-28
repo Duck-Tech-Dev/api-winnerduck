3f8b7c5282ad7e01a067b7bdb6bb61f142eef594
Hüseyin Şimşek <huseyinsimsek7904@gmail.com>
2024-03-28 16:16:29 +0300

    chore: move Table and UserColumn to postgres/database_tables.ts

---
+++ src/postgres/database_tables.ts
@@ -0,0 +1,11 @@
+export enum Table {
+  users = 'users',
+}
+
+export enum UserColumn {
+  id = 'userid',
+  username = 'username',
+  email = 'email',
+  password = 'password',
+  created_at = 'created_at'
+}
