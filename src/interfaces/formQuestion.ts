6d3eab35a2ae4795baedfb02f5a55bf8e824ef80
Hüseyin Şimşek <huseyinsimsek7904@gmail.com>
2024-04-02 14:47:18 +0300

    feat: push the correct FormQuestion interface

--- src/interfaces/formQuestion.ts
+++ src/interfaces/formQuestion.ts
@@ -1,6 +1,23 @@
 export interface FormQuestion {
-  type: string,
-  title: string,
-  description: string,
-  very_important_field: object,
+  // necessary fields
+  title: string;
+  description: string;
+  type: "text" | "number" | "radio" | "checkbox" | "slider" | "email" | "phone" | "date";
+
+  // optional fields
+  isOptional?: boolean;
+
+  // fields that changes based on the type
+  /// text and number
+  minimum?: number;
+  maximum?: number;
+
+  /// radio and checkbox
+  options?: string[];
+
+  /// slider
+  start?: number;
+  end?: number;
+  step?: number;
+  defaultValue?: number;
 }
